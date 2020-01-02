8059da3f69156b3158db796cb218ad91fa361b4f
hp <shivang87gupta@gmail.com>
2020-01-02 21:32:47 +0530

    2nd

--- app/src/main/java/com/myapp/android/smartirrigation/MainActivity.java
+++ app/src/main/java/com/myapp/android/smartirrigation/MainActivity.java
@@ -30,6 +30,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.InstanceIdResult;
+import com.google.firebase.messaging.FirebaseMessaging;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -80,6 +81,7 @@ public class MainActivity extends AppCompatActivity implements DatePickerDialog.
             NotificationManager manager=getSystemService(NotificationManager.class);
             manager.createNotificationChannel(channel);
         }
+        FirebaseMessaging.getInstance().subscribeToTopic("updates");
     }
 
     @Override
--- app/src/main/java/com/myapp/android/smartirrigation/NotificationHelper.java
+++ app/src/main/java/com/myapp/android/smartirrigation/NotificationHelper.java
@@ -1,6 +1,8 @@
 package com.myapp.android.smartirrigation;
 
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
@@ -10,10 +12,22 @@ public class NotificationHelper {
     private static final String CHANNEL_ID="Smart Irrigation";
     private static final String CHANNEL_DESC="Smart Irrigation notification";
     public static void displayNotification(Context context, String text, String title){
+
+        Intent intent=new Intent(context,MainActivity.class);
+
+        PendingIntent pendingIntent=PendingIntent.getActivity(
+                context,
+                100,
+                intent,
+                PendingIntent.FLAG_CANCEL_CURRENT
+        );
+
         NotificationCompat.Builder mbuilder=new NotificationCompat.Builder(context,CHANNEL_ID)
                 .setSmallIcon(R.drawable.common_google_signin_btn_icon_dark)
                 .setContentTitle(title)
                 .setContentText(text)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true)
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT);
 
         NotificationManagerCompat mNotificationmgr=NotificationManagerCompat.from(context);
